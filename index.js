1fe67f88512b1d7bfcff103bf8e5c80e4ecdea02
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-02-23 13:22:38 +0200

    finished both functions

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
-function minMax(...values) {
-  let Min = Infinity;
-  let Max = -Infinity;
+const array = ['HELLO', 122, -10, 'Java', 'JavaScript', 500, 'Nodejs'];
 
-  for(let i = 0; i < values.length; i++) {
-  let value = values[i]
-  if (Array.isArray(value)) {
-    let resultMin = minValue(value);
-    let resultMax = maxValue(value);
-    if (resultMin < Min) Min = resultMin;
-    if (resultMax > Max) Max = resultMax;
-  }
-  else {
-    if (value < Min) Min = value;
-    if (value > Max) Max = value;
+function compNumStr(e1, e2) {
+    let result;
+    const isNum1 = typeof e1 == 'number';
+    const isNum2 = typeof e2 == 'number';
+
+    if (isNum1 && isNum2) {
+        result = e2 - e1;
+    } else if (!isNum1 && !isNum2) {
+        result = e1.localeCompare(e2);
+    } else {
+        result = isNum1 ? -1 : 1;
     }
-  }
-  return [Min, Max];
+    return result;
 }
 
-function minValue(array) {
-  let min = array[0];
-  for(let i = 0; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-      }
-    }
-    return min;
-  }
-
-function maxValue(array) {
-  let max = array[0];
-  for(let i = 0; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i]; 
-    }
-  }
-  return max;
+function orderedList(arr) {
+    const listItems = arr
+        .map(item => {
+            let li;
+            if (typeof item == 'number') {
+                li = `<li class="item item_number">${item}</li>`;
+            } else {
+                li = `<li class="item">${item}</li>`;
+            }
+            return li;
+        })
+        .join('');
+    const result = `<ol>${listItems}</ol>`;
+    return result;
 }
 
-console.log(minMax(1, 2, 3)) // Expected result [1, 3]
-console.log(minMax(1, 2, 3, [100, 50])) // Expected result [1, 100]
-console.log(minMax(1, 2, 3, [100, 50], [-2, 40, 200])) // Expected result [-2, 200]
+const bodyElement = document.querySelector('body');
+bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
+
+const sortedArray = array.sort(compNumStr);
+console.log(sortedArray);
