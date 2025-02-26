d71cabe2873dc82481691194093ab2aef56e9d43
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-02-26 16:16:16 +0200

    finished first function

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
-const array = ['HELLO', 122, -10, 'Java', 'JavaScript', 500, 'Nodejs'];
-
-function compNumStr(e1, e2) {
-    let result;
-    const isNum1 = typeof e1 == 'number';
-    const isNum2 = typeof e2 == 'number';
-
-    if (isNum1 && isNum2) {
-        result = e2 - e1;
-    } else if (!isNum1 && !isNum2) {
-        result = e1.localeCompare(e2);
-    } else {
-        result = isNum1 ? -1 : 1;
+function myReduce(array, callback, initialValue) {
+    let accumulator = initialValue == undefined ? array[0] : initialValue
+    const startIndex = initialValue == undefined ? 1 : 0
+    for (let i = startIndex; i < array.length; i++) {
+      accumulator = callback(accumulator, array[i], i, array);
     }
-    return result;
-}
-
-function orderedList(arr) {
-    const listItems = arr
-        .map(item => {
-            let li;
-            if (typeof item == 'number') {
-                li = `<li class="item item_number">${item}</li>`;
-            } else {
-                li = `<li class="item">${item}</li>`;
-            }
-            return li;
-        })
-        .join('');
-    const result = `<ol>${listItems}</ol>`;
-    return result;
-}
-
-const bodyElement = document.querySelector('body');
-bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
-
-const sortedArray = array.sort(compNumStr);
-console.log(sortedArray);
-
-const array2 = ["Banana", 200, "Apple", -50, "Orange", 350];
-console.log("array2:", array2.sort(compNumStr));
-
-const array3 = [12, 40, 23, 44, 1];
-console.log("array3:", array3.sort(compNumStr));
+    return accumulator;
+  }
+  
