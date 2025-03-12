5027dccc69c5458161745c045489a9ead3e69163
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-03-12 13:31:45 +0200

    added more tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const scripts = `
 const company = new Company();
 const emp1 = new Employee("Vasya", 1000, "HR");
 const emp2 = new Employee("Ivan", 1200, "HR");
+const emp3 = new Employee("Dima", 1300, "HR");
 const wageEmp1 = new WageEmployee("Andrey", 800, "IT", 10, 50);
 const wageEmp2 = new WageEmployee("Alex", 900, "IT", 5, 60);
 const manager1 = new Manager("David", 1500, "Sales", 1.5);
@@ -19,6 +20,7 @@ company.addEmployee(manager2);
 testframework('Company Class Tests', scripts,
   [
     'Object.keys(company.employees).length',
+    'company.addEmployee(emp3); Object.keys(company.employees).length', 
     'company.deleteEmployee(emp1); Object.keys(company.employees).length',
     'company.getDepBudget("HR")',
     'company.getDepBudget("IT")',
@@ -26,7 +28,8 @@ testframework('Company Class Tests', scripts,
     'company.getEmployeesDepartment("IT").length'
   ],
   [
-    6,              // amount of employees added Expected: 6
+    6,              // amount of employees Expected: 6
+    7,              // amount of employees after addition Expected: 7
     5,              // amount of employees after deletion Expected: 5
     2200,           // HR department budget Expected: 2200
     5500,           // IT department budget Expected: 5500
