d8aaff47201c69bdb843ffa34f87898b2e3b165d
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-03-19 13:23:40 +0200

    Finished password and username functions

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 function getUserPassword(probCorrectPass) {
-  //example what should be done in the HW solution
   const passwords = ['correct', 'wrong'];
   const index =  Math.random() < probCorrectPass ? 0 : 1
   return new Promise(resolve => {
-    setTimeout(() => resolve(passwords[index]));
+    setTimeout(() => resolve(passwords[index]), 1000);
   })
 }
 function login(password) {
-  //TODO
-  //returns promise in the state resolved only for passowrd 'correct' otherwise state rejected
-}
-function getUserData(username) {
-  const users = {'Vasya': {name: "Vasya", age: 30},
-  'Petya': {name: "Petya", age:40}}
-  //TODO
-  //returns promise in the state resolved if username exists with returning user data
-  //otherwise state rejected with apropriate message
+  return new Promise((resolve, reject) => setTimeout(() => {
+    (password === 'correct') ? resolve('Login successful') : reject('Invalid password')}, 2000))}
 
-  }
-function funStackExample(username) {
-  // const password = getUserPassword(0.8);
-  // try {
-  //   login(password);
-  //   const userData = getUserData(username);
-  //   console.log(userData);
-  // } catch (error) {
-  //   console.log(`error: ${error}`)
-  // }
-  //runs the same functionality as commented out above but with calling of asynchronous functions
-}
+    function getUserData(username) {
+  const users = {
+    'Vasya': {name: "Vasya", age: 30},
+  'Petya': {name: "Petya", age:40}}
+ 
+  return new Promise((resolve, reject) => {
+    setTimeout (() => {
+      (users[username]) ? resolve(users[username]) : reject(`User ${username} not found`)}, 1000)})
+    };
+
+    function funStackExample(username) {
+
+    }
+
 funStackExample('Vasya')
+
